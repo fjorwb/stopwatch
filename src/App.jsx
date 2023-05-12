c75ce699b39f46f12f011e08a3720c12d5621c66
FJORWB <88600252+fjorwb@users.noreply.github.com>
2023-05-12 12:05:09 -0500

    last commit - toggle lap viewer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { useContext } from 'react'
 import './App.css'
+
+import { Context } from './context/context'
 
 import Stopwatch from './components/stopwatch'
 import Laps from './components/laps'
 
 export default function App () {
+  const { showLap } = useContext(Context)
+
   return (
     <>
       <h1 className='title'>STOPWATCH</h1>
       <div className='container'>
         <Stopwatch />
-        <Laps />
+        {showLap && <Laps />}
       </div>
     </>
   )
